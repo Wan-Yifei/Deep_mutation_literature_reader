d12d394c2ee70bd4caa8459051e97be31b12440a
Wan-Yifei <wanyifei0123@gmail.com>
2021-09-06 00:55:42 -0400

    Feat: Toolkit for HASH message authentication File: hmac_signature.py
    1. Generate signature;
    2. Verify signature.

---
+++ hmac_signature.py
@@ -0,0 +1,34 @@
+import hmac
+import hashlib
+
+
+def generate_hmac_signature(obj, key):
+    """
+    Generate HASH message signature for object.
+
+    :param obj: bytes or bytearry object needs a HASH authentication;
+    :param key: key for generating signature;
+    :return: hmac_signature: HASH signature for provided object with specified key.
+    """
+    if not isinstance(key, (bytes, bytearray)):
+        key = str.encode(key, encoding="utf8")
+    hmac_signature = hmac.new(key, obj, hashlib.sha224).hexdigest()
+    return hmac_signature
+
+
+def verify_hmac_signature(obj, key, signature_old):
+    """
+    Verify HASH message signature.
+
+    :param obj: bytes project used for generating signature;
+    :param key: key for generating signature;
+    :param signature_old: expected signature;
+    :return: verification: bool, True (verification pass) or False (verification failed).
+    """
+    signature_new = generate_hmac_signature(obj, key)
+    if hmac.compare_digest(signature_new, signature_old):
+        verification = True
+    else:
+        verification = False
+        print("Integrity check failed!")
+    return verification
