73d8f0e270492b5c73fbfb1999a76394f3124985
Wan-Yifei <wanyifei0123@gmail.com>
2021-09-10 15:46:17 -0400

    Feat: Add support of tmVar mutation NER File: hgvs_variants_capture.py
    1. Use entities from tmVar to structure regular expression;
    2. Map tmVar variant entities to placeholders;
    3. Combine conventional regex method with tmVar based method.

--- hgvs_variants_capture.py
+++ hgvs_variants_capture.py
@@ -29,10 +29,30 @@ def map_temp_encoding(key, key_2_placeholder, placeholder_prefix, notice):
     """
     index = len(key_2_placeholder)
     value = placeholder_prefix + str(index)
+    key = key.strip(" ,.)")  # remove following space, comma, period and parentheses matched by \s.
     if key not in key_2_placeholder:
         key_2_placeholder[key] = value
     if notice:
         print("key {} has been mapped to temporary encoding {}!".format(key, key_2_placeholder[key]))
+
+
+def escape_character_variant(variant):
+    """
+    Add backslash to special characters to convert a variant name string into a regex pattern.
+
+    :param variant: string, variant;
+    :return: variant_pattern: regex variant pattern.
+    """
+    special_characters = '''{}[]()^$.|*+?\\'''  # special chars from string.punctuation
+    var_characters = []
+    for char in variant:
+        if char in special_characters:
+            var_characters.append(char.replace(char, r"\{}".format(char)))
+        else:
+            var_characters.append(char)
+    variant_pattern = "".join(var_characters) + r".*?(?:\s|$)"
+    variant_pattern = variant_pattern.lower()
+    return variant_pattern
 
 
 def recognize_variant_entity(variant_type, text, pattern=default_var_pattern):
@@ -50,18 +70,33 @@ def recognize_variant_entity(variant_type, text, pattern=default_var_pattern):
     return recognized_entities
 
 
-def build_var_temp_encoding(recognized_entities, placeholder_prefix="temp_placeholder_", notice=False):
+def get_variant_entity_tmvar(text, ner):
+    assert (isinstance(ner, list)), "Please check your ner arguments."
+    recognized_entities = {}  # store all match words
+    n = 0
+    # the ner list includes variants entities recognized by other tool
+    for variant in ner:
+        regex = escape_character_variant(variant)
+        vartype = "tmvar_" + str(n)
+        recognized_entities[vartype] = re.finditer(regex, text)  # variant entities from tmVar tool
+        n += 1
+    return recognized_entities
+
+
+def build_var_temp_encoding(recognized_entities, key_2_placeholder, placeholder_prefix="temp_placeholder_", notice=False):
     """
     Map variant to placeholder (temporary encoding).
 
     :param recognized_entities: dictionary, key -> variant type, value -> generator of regex match results;
+    :param key_2_placeholder: dictionary, key -> variant, value -> placeholder, store mapping between key/placeholder;
     :param placeholder_prefix: string, prefix of placeholder, by default "temp_placeholder_";
     :param notice: bool, if True, print out the dictionary update process for each key;
     :return: key_2_placeholder, dictionary: key -> variant, value -> placeholder.
     """
-    key_2_placeholder = {}  # dict: variant -> placeholder
-    for variants in recognized_entities.values():
+    for key, variants in recognized_entities.items():
         [map_temp_encoding(var.group(0), key_2_placeholder, placeholder_prefix, notice) for var in variants]
+    if notice:
+        print(key_2_placeholder)
     return key_2_placeholder
 
 
@@ -130,6 +165,55 @@ def test(build_notice, key2pla_notice, pla2key_notice):
     replace_temp_encoding_2_var(key_2_placeholder, new_text, notice=pla2key_notice)
 
 
+def test2(build_notice, key2pla_notice, pla2key_notice):
+    """
+    Integrated test2. Test both regex method and tmVar method.
+
+    :param build_notice: bool, if True, print out the dictionary update process for each key;
+    :param key2pla_notice: bool, if True, print out the replace process for each variant;
+    :param pla2key_notice: bool, if True, print out the replace process for each variant;
+    :return: None
+    """
+    text = "c.224g>a, p.arg75gln (r75q) presumably leads to an amino-acid change from arginine to glutamine in the " \
+           "membrane-spanning domain of the cftr protein. initially reported as a benign sequence variation, " \
+           "p.arg75gln was associated with a high risk of pancreatitis, a risk that was strikingly higher " \
+           "when p.arg75gln was combined with a spink1 variant. in addition, it was shown that p.arg75gln alters " \
+           "bicarbonate but not chloride conductance and that the mutation also induces exon 3 skipping. to " \
+           "investigate the role of p.arg75gln in idiopathic chronic pancreatitis (icp), we performed genotyping of " \
+           "the cftr gene in 880 patients with icp, 198 patients with idiopathic bronchiectasis (ib), 74 patients " \
+           "with classical cystic fibrosis (cf), 48 patients with congenital bilateral absence of the vas deferens (" \
+           "cbavd) and 148 healthy controls. p.arg75gln was identified in 3.3% (29/880) of patients with icp, " \
+           "3.3% (9/272) patients with a pulmonary disease, 2.1% (1/48) of patients with cbavd and 4.7% (7/148) of " \
+           "healthy controls. it was frequently associated with the c.[1210-12t[7];1408a>g] (t7-p.val470) allele and " \
+           "this cftr genetic background could not explain the putative pathogenicity of this variant. to assess " \
+           "whether cftr and spink1 mutations are co-inherited in pancreatitis, we sequenced spink1 gene exon 3 in " \
+           "the 46 patients who were previously identified to be heterozygous for p.arg75gln. two spink1 " \
+           "pancreatitis-associated variants, p.asn34ser and p.pro55ser, were found in 6 patients: 4 of 29 (13.8%) " \
+           "patients with icp (3 p.asn34ser and 1 p.pro55ser), 1 of 7 (14.3%) healthy controls (p.asn34ser) and 1 of " \
+           "9 (11.1%) patients with ib (p.pro55ser). our study does not confirm that the cftr p.arg75gln mutation " \
+           "confers a significant risk of pancreatitis when considered individually and with a concurrent spink1 " \
+           "mutation, suggesting the role of other genetic and environmental factors. "
+    tmvar_ner = ["p.Arg75Gln", "p.Pro55Ser", "c.[1210-12T[7]", "p.Asn34Ser", "R75Q"]
+    print("Original text: \n{}".format(text))
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    tmvar_entities = get_variant_entity_tmvar(text, tmvar_ner)
+    key_2_placeholder_init = {}
+    # Build tmVar based key to placeholder mapping
+    key_2_placeholder_tmvar = build_var_temp_encoding(tmvar_entities, key_2_placeholder_init, notice=build_notice)
+    tmvar_text = replace_var_2_temp_encoding(key_2_placeholder_tmvar, text, notice=key2pla_notice)  # tmVar replacement
+    print("Text after tmVar replacement: \n{}".format(tmvar_text))
+    regex_entities = recognize_variant_entity("dna", tmvar_text)  # conventional regex NER
+    # Combine regex based key2placeholder with tmVar based key2placeholder
+    key_2_placeholder = build_var_temp_encoding(regex_entities, key_2_placeholder_tmvar, notice=build_notice)
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    regex_tmvar_text = replace_var_2_temp_encoding(key_2_placeholder, tmvar_text, notice=key2pla_notice)
+    new_text = replace_temp_encoding_2_var(key_2_placeholder, regex_tmvar_text, notice=pla2key_notice)
+    print("Text after both tmVar and regex based replacement: \n{}".format(regex_tmvar_text))
+    assert (text == new_text), "Error: New text does not match the original text."
+    print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+    print("\nNew text matches the original text! Test passed!")
+
+
 if __name__ == "__main__":
     try:
         print_build = sys.argv[1]
@@ -138,5 +222,5 @@ if __name__ == "__main__":
     except IndexError:
         print_build = False
         print_key2pla = False
-        print_pla2key = True
-    test(print_build, print_key2pla, print_pla2key)
+        print_pla2key = False
+    test2(print_build, print_key2pla, print_pla2key)
