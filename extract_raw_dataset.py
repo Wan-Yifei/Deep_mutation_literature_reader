44457c2a08325b7c72390261081713c54079dc6e
Wan-Yifei <wanyifei0123@gmail.com>
2021-11-17 00:59:38 -0500

    Feat: Get pmids, titles and sources (labels) from MVL record File: extract_raw_dataset.py 1. Extract data from CSV file; 2. Pickle result.

--- extract_raw_dataset.py
+++ extract_raw_dataset.py
@@ -1,5 +1,6 @@
 import sys
 import csv
+from pickle_toolbox import *
 
 
 def retrieve_pmid_title(input_file):
@@ -20,7 +21,6 @@ def retrieve_pmid_source(input_file):
         header = [colname.lower() for colname in header]
         pmid_ind = header.index("pmid")
         source_ind = header.index("source")
-        tmp = {}
         pmid_source = {}
         for row in raw:
             pmid_source[row[pmid_ind]] = list(set(pmid_source.get(row[pmid_ind], []) + [row[source_ind]]))
@@ -29,8 +29,21 @@ def retrieve_pmid_source(input_file):
 
 def main():
     input_file = sys.argv[1]
+    output_pmid_title = sys.argv[2]
+    output_pmid_source = sys.argv[3]
+    try:
+        output_hmac_key = sys.argv[4]
+    except IndexError:
+        print("Notice: No HMAC key provided!")
+        output_hmac_key = None
     pmid_title = retrieve_pmid_title(input_file)
     pmid_source = retrieve_pmid_source(input_file)
+    if output_hmac_key:
+        pickle_output(pmid_title, output_pmid_title, output_folder=".", key=output_hmac_key, use_hmac=True)
+        pickle_output(pmid_source, output_pmid_source, output_folder=".", key=output_hmac_key, use_hmac=True)
+    else:
+        pickle_output(pmid_title, output_file=output_pmid_title, output_folder=".")
+        pickle_output(pmid_source, output_file=output_pmid_source, output_folder=".")
 
 
 if __name__ == '__main__':
